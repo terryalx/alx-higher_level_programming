8af48c0b2e37bad7a708c44a603609cfaa953c00
setalxswe <setalxswe@gmail.com>
2023-09-05 20:11:40 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        if 97 <= ord(char) <= 122:
+        if ord(char) => 97 and ord(char) <= 122:
             print("{:c}".format(ord(char) - 32), end="")
         else:
             print("{:c}".format(ord(char)), end="")
