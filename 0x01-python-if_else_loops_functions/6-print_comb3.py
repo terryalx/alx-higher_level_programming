8bb27de31dc8513cfa49bde8c1ecc67b538b4c35
setalxswe <setalxswe@gmail.com>
2023-09-05 19:51:38 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,3 +5,4 @@ for tens_digit in range(10):
             print("{:d}{:d}, ".format(tens_digit, ones_digit), end='')
         else:
             print("{:d}{:d}".format(tens_digit, ones_digit))
+print()
