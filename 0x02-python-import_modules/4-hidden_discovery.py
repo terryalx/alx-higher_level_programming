0b7c2bafd2a74c1e8787f5951dae8a89bfadbd51
setalxswe <setalxswe@gmail.com>
2023-09-07 12:43:36 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    d = dir
+    d = dir(hidden_4)
     for i in range(0, len(d)):
         if d[i][0:2] != "__":
             print("{}".format(d[i]))
