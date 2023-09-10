146464ab8234f827100df4aec8beb343bbf497b8
setalxswe <setalxswe@gmail.com>
2023-09-09 23:33:56 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
 
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string += char
+            new_string == my_string
 
     return new_string
