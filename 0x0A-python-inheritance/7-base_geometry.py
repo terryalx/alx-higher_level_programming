e97575ad3709583ba92a752443bf59899a24385a
setalxswe <setalxswe@gmail.com>
2023-10-10 04:19:23 -0700

    :

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -26,7 +26,7 @@ class BaseGeometry():
             ValueError: when below 1
         """
 
-        if not isinstance(value, int)
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
