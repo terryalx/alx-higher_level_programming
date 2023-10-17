531ed2a6f6c68ffc19941537d0c05304328d4cfe
setalxswe <setalxswe@gmail.com>
2023-10-16 17:37:45 -0700

    __square__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-"""Write a child class Square"""
+"""
+Write a child class Square
+"""
 
 from models.rectangle import Rectangle
 
@@ -15,52 +16,65 @@ class Square(Rectangle):
         id: id
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """class constructor"""
+        """
+        class constructor
+        """
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        """Returns a string representation of a square instance"""
+        """
+        Returns a string representation of a square instance
+        Prints [Square] (<id>) <x>/<y> - <size>
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        """Retrieve attribute from class Rectangle"""
+        """
+        Retrieve attribute from class Rectangle
+        getter function for width
+        Returns: width
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """validation from class Rectangle"""
+        """
+        validation from class Rectangle
+        width and the height - with the same value
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """assigns an argument to each attribute"""
-        if len(args):
-            for i, a in enumerate(args):
-                if i == 0:
-                    self.id = a
-                elif i == 1:
-                    self.size = a
-                elif i == 2:
-                    self.x = a
-                elif i == 3:
-                    self.y = a
-        else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+        """
+        assigns key/value argument to attributes
+        kwargs is skipped if args is not empty
+            Args:
+                *args -  variable number of no-keyword args
+                **kwargs - variable number of keyworded args
+        """
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
 
     def to_dictionary(self):
-        """Returns dict representation of a Rectangle"""
+        """
+        Returns dict representation of a Rectangle
+        """
         return {
-            "id": self.id,
-            "x": self.x,
-            "size": self.size,
-            "y": self.y,
-            }
+                "id": self.id,
+                "x": self.x,
+                "size": self.size,
+                "y": self.y,
+                }
