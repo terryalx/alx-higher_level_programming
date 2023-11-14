02fa6fde9bf7c4196924fdbd509fe614c979453b
setalxswe <setalxswe@gmail.com>
2023-11-14 14:22:06 -0800

    __SHOW__

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert values
+-- first_table (database hbtn_0c_0)
+-- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
