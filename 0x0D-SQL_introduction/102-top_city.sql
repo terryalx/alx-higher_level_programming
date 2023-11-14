c28ac427b217bd85ce3b995409bc3822dbcbefd1
setalxswe <setalxswe@gmail.com>
2023-11-14 14:42:45 -0800

    __SHOW__

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- Execute: cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
