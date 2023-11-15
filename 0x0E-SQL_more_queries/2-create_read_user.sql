89d87ad3246abc4ceaed4dc6928e769cbfd50d97
setalxswe <setalxswe@gmail.com>
2023-11-15 04:14:40 -0800

    defualt=1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,7 @@
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
+-- Execute: cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,4 +2,5 @@
 -- force_name description: id INT, name VARCHAR(256) can’t be null
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
+-- Execute: cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
