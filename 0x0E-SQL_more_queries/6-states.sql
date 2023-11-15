f822aeb0abd75bb87f1277d255be7575ea180765
setalxswe <setalxswe@gmail.com>
2023-11-15 04:24:18 -0800

    UTO_INCREMENT NOT NULL UNIQUE,

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- states description:
+-- 	  id INT unique, auto generated, can’t be null and is a primary key
+--	  name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table states already exists, your script should not fail
+-- Execute: cat 6-states.sql | mysql -hlocalhost -uroot -p 
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states`
+(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	name VARCHAR(256)
+);
