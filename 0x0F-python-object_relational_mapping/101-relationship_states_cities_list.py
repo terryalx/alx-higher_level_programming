bc6051a8aed3141fe162c49a4dad20ea29339286
setalxswe <setalxswe@gmail.com>
2023-12-16 19:11:14 -0800

    task

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+
 """
-Lists all State objects and corresponding City objects contained in the DB
+Module that connects python script to a database
 """
-import sys
-from relationship_state import Base, State
-from relationship_city import City
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import Base, City
+from relationship_state import State
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
 
-    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
-
-    for state in st:
+    for state in my_session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
+
+    my_session.close()
