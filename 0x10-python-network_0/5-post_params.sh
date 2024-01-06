bb2c8ee41dd2bbab0cb066afa5e1d5a8ae296320
setalxswe <setalxswe@gmail.com>
2024-01-05 20:56:23 -0800

    post

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# takes in a URL
+# sends a POST request to the passed URL
+# displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
